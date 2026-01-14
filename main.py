e59cfceda5731dced49497c2b98f4ba9e62c66fd
Fabienne Kuntz <fabienne.kuntz04@gmail.com>
2026-01-14 15:19:58 +0100

    Add standard inventory and menu function

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-from products import Product
-from store import Store
+import products
+import store
 
-def main():
-    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-    mac = Product("MacBook Air M2", price=1450, quantity=100)
+product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                products.Product("Google Pixel 7", price=500, quantity=250)
+                ]
+best_buy = store.Store(product_list)
 
-    print(bose.buy(50))
-    print(mac.buy(100))
-    print(mac.is_active())
+def start(store):
+    user_input = None
+    print("   Store Menu")
+    print("   ----------")
+    while user_input != 4:
+        print("1. List all products in store")
+        print("2. Show total amount in store")
+        print("3. Make an order")
+        print("4. Quit")
+        user_input = int(input("Please choose a number: "))
+        print("------")
 
-    bose.show()
-    mac.show()
 
-    bose.set_quantity(1000)
-    bose.show()
+start(best_buy)
 
-    product_list = [Product("MacBook Air M2", price=1450, quantity=100),
-                    Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                    Product("Google Pixel 7", price=500, quantity=250),
-                    ]
 
-    best_buy = Store(product_list)
-    products = best_buy.get_all_products()
-    print(best_buy.get_total_quantity())
-    print(best_buy.order([(products[0], 1), (products[1], 2)]))
 
-if __name__ == "__main__":
-    main()
+
